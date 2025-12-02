4e21a7d5253cd6ccb023bf26bf3c03c3f839c1ea
76Claudia <claudinha.gcruz@gmail.com>
2025-12-01 23:02:39 -0300

    Test findAll ok

--- src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
@@ -1,9 +1,26 @@
 package com.devsuperior.dsmovie.services;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.tests.MovieFactory;
 
 @ExtendWith(SpringExtension.class)
 public class MovieServiceTests {
@@ -11,8 +28,37 @@ public class MovieServiceTests {
 	@InjectMocks
 	private MovieService service;
 	
+	@Mock
+	private MovieRepository repository;
+	private long existingMovieId, nonExistingMovieId;
+	private String movieTitle;
+	private MovieEntity movie;
+	private PageImpl<MovieEntity> page;
+	
+	@BeforeEach
+	void setUp()throws Exception{
+		movieTitle = "Test Movie";
+		
+		movie = MovieFactory.createMovieEntity();
+		
+		page = new PageImpl<>(List.of(movie));
+		
+	
+		Mockito.when(repository.searchByTitle(any(), (Pageable)any())).thenReturn(page);
+		
+		
+	}
+	
 	@Test
 	public void findAllShouldReturnPagedMovieDTO() {
+		
+		Pageable pageable = PageRequest.of(0, 12);
+		String title = "Test Movie";
+		Page<MovieDTO> result = service.findAll(movieTitle, pageable);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getSize(), 1);
+		Assertions.assertEquals(result.iterator().next().getTitle(), movieTitle);
 	}
 	
 	@Test
